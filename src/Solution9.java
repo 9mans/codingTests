8a81bb66155ecbd476708a7734bf114b8436277e
rbska <rbska1234@naver.com>
2024-04-23 19:41:36 +0900

    프로그래머스 코딩테스트

---
+++ src/Solution9.java
@@ -0,0 +1,26 @@
+public class Solution9 {
+    public int solution(int n) {
+        int answer = 0;
+
+        if (n <= 7) {
+            answer = 1;
+        } else if (n >= 7) {
+            if (n % 7 == 0) {
+                answer = n / 7;
+            } else {
+                answer = n / 7 + 1;
+            }
+
+
+        }
+
+
+
+
+
+
+
+        return answer;
+    }
+
+}
