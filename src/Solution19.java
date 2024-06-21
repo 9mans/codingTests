24caf453c0979542258f7d97cb6e57c8eec79936
rbska <rbska1234@naver.com>
2024-06-21 20:14:27 +0900

    프로그래머스 코딩테스트

---
+++ src/Solution19.java
@@ -0,0 +1,14 @@
+public class Solution19 {
+    public int[] solution(int[] array) {
+        int[] answer = {0, 0};
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (answer[0] < array[i]) {
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+        return answer;
+    }
+}
